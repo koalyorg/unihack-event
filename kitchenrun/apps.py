a84340739629273356a9658458903f11c5b285a2
Jonas Hönge <jonas@hoenge.com>
2023-11-17 14:39:24 +0200

    Add Kitchenrun and links

---
+++ kitchenrun/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KitchenrunConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "kitchenrun"
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    if request.user.is_authenticated:
+        context = {}
+        return render(request, 'dashboard.html', context)
+    else:
+        return render(request, 'index.html')
