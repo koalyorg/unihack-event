6b6b293799610e188f40c3ee07499da3ebb508c8
Leon Leisten <leon.leisten@tu-ilmenau.de>
2023-11-18 20:02:40 +0100

    map style changes

--- static/map/map.js
+++ static/map/map.js
@@ -2,8 +2,9 @@
 var map = L.map('map').setView(destination, 14);
 L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
 	maxZoom: 18,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	attribution: '&copy; <a href="openstreetmap.org/copyright">OpenStreetMap</a> contributors | <a href="www.graphhopper.com">GraphHopper</a> | <a href="nominatim.org">Nomatim</a>'
 }).addTo(map);
+L.marker(destination).addTo(map);
 
 // Track User
 var user_location = undefined;
@@ -11,16 +12,18 @@ var first_tracking = true;
 function trackLocation() {
     map.locate();
 }
-console.log(map);
 function onLocationFound(e) {
-    console.log("Location found")
     L.circleMarker(e.latlng, 10).addTo(map);
     user_location = [e.latitude, e.longitude];
     if (first_tracking) {
         first_tracking = false;
-        calculateRoute();
+        calculateRoute(); // alt.: setInterval(...)
     }
-    setTimeout(trackLocation, 5000); // alt.: setInterval()
+    setTimeout(trackLocation, 5000);
+}
+function onLocationError(e) {
+    console.log("Error" + e.message);
+    setTimeout(trackLocation, 2000)
 }
 map.on('locationfound', onLocationFound);
 map.locate({setView: true});
@@ -28,20 +31,16 @@ map.locate({setView: true});
 // Routing with Graphhopper
 // 11/18/2013: Free version has 15,000 requests per day
 function calculateRoute() {
-    if (!user_location) return; // Break if locating failed
-    console.log("Routing...");
+    if (!user_location) return;
+    if (!apikey) return;
     L.Routing.control({
         waypoints: [
             L.latLng(user_location[0], user_location[1]),
             L.latLng(destination[0], destination[1])
         ],
-        router: L.Routing.graphHopper(apikey, {
-            urlParameters: {vehicle: 'car'}
-        }),
+        router: L.Routing.graphHopper(apikey, { urlParameters: {vehicle: 'foot'} }),
         autoRoute: false, // To prevent excessive use of ressources
-        createMarker: function() { // Only show destination marker
-            L.marker(destination).addTo(map);
-            return null;
-        },
+        createMarker: function() { return null; }, // Removes default markers
+        lineOptions: { styles: [{color: 'red', weight: 5}] },
     }).addTo(map).route();
 }
